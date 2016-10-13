f942e65e5912eae16e934ecc1e29135aa6462c98
Joshua Walker <jwtechdev@gmail.com>
2016-10-13 15:39:17 -0400

    "initial commit w/ using dictionaries"

---
+++ amazing_movies_and_dictionaries.py
@@ -0,0 +1,8 @@
+#using movies to practice writing dictionaries
+movies = {'action': ['Expendables','The Quest','Fist of Legend'],
+        'romance': ['50 First Dates','The Notebook','Titanic'],
+        'weird_azz_stuff': ['Requiem for a Dream','Donnie Darko','Butterfly Effect']}
+
+print('These are the movies in the action category:\n', movies['action'])
+print('These are the movies in the action category:\n', movies['romance'])
+print('These are the movies in the action category:\n', movies['weird_azz_stuff'])
