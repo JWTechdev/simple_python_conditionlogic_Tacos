05ed34d72c81b16ce9c89dcc605b9b052a002847
Joshua Walker <jwtechdev@gmail.com>
2016-10-15 11:03:39 -0400

    initial commit for name_age script

---
+++ name_age.py
@@ -0,0 +1,9 @@
+# This Program says hello and asks for name
+print('Hello World')
+print('whats your name?') # ask for their name
+myName = input()
+print('It is good to meet you ' + myName)
+print(len(myName))
+print('What is your age?') # ask for age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
